b9ed910a86136e1b9780694485e4a1fbdb304fd1
Matt <mattfilmer@gmail.com>
2016-04-02 10:05:14 -0400

    Reworked initial commit

---
+++ app/src/main/java/com/example/siwtw/sayitwiththeirwords/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.siwtw.sayitwiththeirwords;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        verifyStoragePermissions(this);
+
+        final String soundRootDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath() + "/SIWTW";
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, soundRootDirectory, duration);
+        toast.show();
+
+        //final String soundRootDirectory = "/storage/emulated/0/Music/SIWTW";
+        ListView lv;
+        final ArrayList<String> FilesInFolder = GetFiles(soundRootDirectory);
+        lv = (ListView)findViewById(R.id.soundsListView);
+
+        lv.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, FilesInFolder));
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                playChosenSound(soundRootDirectory + "/" + FilesInFolder.get(position));
+            }
+        });
+    }
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE//,
+            //Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    public ArrayList<String> GetFiles(String DirectoryPath) {
+        ArrayList<String> MyFiles = new ArrayList<String>();
+        File f = new File(DirectoryPath);
+
+        f.mkdirs();
+        File[] files = f.listFiles();
+        if (files.length == 0)
+            return null;
+        else {
+            for (int i=0; i<files.length; i++)
+                MyFiles.add(files[i].getName());
+        }
+
+        return MyFiles;
+    }
+
+    public void playChosenSound(String audioPathName)
+    {
+        MediaPlayer mp = new MediaPlayer();
+        try {
+            mp.setDataSource(audioPathName);
+            mp.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mp.start();
+    }
+
+    public void playTestSound(View v)
+    {
+        MediaPlayer mp = new MediaPlayer();
+
+        //String testAudioFileName = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath() + "/flimflams.ogg";
+        //String testAudioFileName = "/data/media/Music/flimflams.ogg";
+        String testAudioFileName = "/storage/emulated/0/Music/SIWTW/I Noticed You Left out Film Flams.wav";
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, testAudioFileName, duration);
+        toast.show();
+
+        try {
+            mp.setDataSource(testAudioFileName);
+            mp.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mp.start();
+    }
+}
